bd8dc94de9bc03419691fb62acfc013495054db9
setin82@gmail.com <setin82@gmail.com>
2016-01-25 23:31:21 +0900

    Test

---
+++ puzzler/src/main/java/com/setin/puzzler/ch02/Expression.java
@@ -0,0 +1,17 @@
+package com.setin.puzzler.ch02;
+
+/**
+ * Created by 두석 on 2016-01-25.
+ */
+public class Expression {
+   /* public static boolean isOdd(int i) {
+        return i & 2 == 1;
+    }*/
+
+
+    public static boolean isOdd(int i ){
+        return (i & 1) != 0;
+    }
+
+
+}
